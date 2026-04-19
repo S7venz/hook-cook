abacaccc70224a6aa2f41e6ba2aac19fdd4d0fc7
S7venz <cengizhanozbek02@gmail.com>
2026-04-19 15:05:52 +0200

    fix(cors): scan backend.config package pour activer CorsConfig

--- backend/grails-app/init/backend/Application.groovy
+++ backend/grails-app/init/backend/Application.groovy
@@ -2,10 +2,11 @@ package backend
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-
 import groovy.transform.CompileStatic
+import org.springframework.context.annotation.ComponentScan
 
 @CompileStatic
+@ComponentScan(['backend', 'backend.config'])
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
